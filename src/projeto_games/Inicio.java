f7390615b74a1f0899cbd208a60b178e81bfb1e0
Lucas Bastos <lucas.gbastos@hotmail.com>
2021-06-03 18:50:36 -0300

    modificações na classe Inicio: criação do método menu, para tratar caos que o usuario coloque algo diferente de um inteiro.

--- src/projeto_games/Inicio.java
+++ src/projeto_games/Inicio.java
@@ -2,49 +2,64 @@ package projeto_games;
 
 import java.util.Scanner;
 
-public class Inicio {
-	private static Scanner sc = new Scanner(System.in);
+import static java.lang.System.in;
 
+// TODO: 03/06/2021 implementar System.exit(1), no case 4, para terminar a execução e retirar o do-while da main
+public class Inicio {
 	public static void main(String[] args) {
 
-		int option = 0;
+		int option;
 
 		do {
-			impressao("-----------------------------------------");
-			impressao("Qual jogo você deseja jogar?");
-			impressao("(1) Jogo da Forca\n(2) Batalha Naval\n(3) Jogo da Velha\n(4) Sair");
-			option = sc.nextInt();
+			option = menu();
 			caseOptions(option);
 		} while (option != 4);
+
+	}
+
+	private static int menu(){
+		int i;
+		impressao("-----------------------------------------");
+		impressao("Qual jogo você deseja jogar?");
+		impressao("(1) Jogo da Forca\n(2) Batalha Naval\n(3) Jogo da Velha\n(4) Sair");
+		Scanner scan = new Scanner(in);
+		try{
+			i = scan.nextInt();
+		} catch (Exception ie){
+			impressao("Por favor, insira um número:");
+			i = menu();
+		}
+		return i;
 
 	}
 
 	private static void caseOptions(int option) {
 		switch (option) {
 		case 1:
-			System.out.println(" \n" + " ");
-			System.out.println("JOGO DA FORCA INICIADO\n" + "-----------------------------");
+			impressao(" \n" + " ");
+			impressao("JOGO DA FORCA INICIADO\n" + "-----------------------------");
 			Forca.inicio();
 			break;
 
 		case 2:
-			System.out.println(" \n" + " ");
-			System.out.println("BATALHA NAVAL INICIADO\n" + "-----------------------------");
+			impressao(" \n" + " ");
+			impressao("BATALHA NAVAL INICIADO\n" + "-----------------------------");
 			BatalhaNaval.inicio();
 			break;
 
 		case 3:
-			System.out.println(" \n" + " ");
-			System.out.println("JOGO DA VELHA INICIADO\n" + "-----------------------------");
+			impressao(" \n" + " ");
+			impressao("JOGO DA VELHA INICIADO\n" + "-----------------------------");
 			JogoDaVelha.inicio();
 			break;
 
 		case 4:
-			System.out.println("Você saiu do jogo.");
+			impressao("Você saiu do jogo.");
+//			System.exit(1);
 			break;
 
 		default:
-			System.out.println("Número inválido");
+			impressao("Número inválido");
 			break;
 		}
 	}
